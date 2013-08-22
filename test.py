1f735e90933be0c5df1453a6eaf4be24cce165e2
Clara Graf <clara@graf-simpson.at>
2013-08-22 11:47:31 +0200

    Update test.py
    Schreibweise verbessert.

--- test.py
+++ test.py
@@ -6,19 +6,19 @@ versuche=0
 while True :
     versuche=versuche+1
     if versuche>3:
-        easygui.msgbox("Du hast alle versuche verbraucht")
+        easygui.msgbox("Du hast alle Versuche verbraucht")
         break
-    antwort=easygui.buttonbox("Wo ist die kugel","Finde die Kugel: versuch {}".format(versuche),
-                  ["Klick mich","Nein hör nicht auf ihn klick mich",
-                   "Wenn du mich klickst wirst du es schaffen","Komm zu mir"],image="sea-serpent.gif")
-    richtig=random.choice(["Klick mich","Nein hör nicht auf ihn klick mich",
-                   "Wenn du mich klickst wirst du es schaffen","Komm zu mir"])
+    antwort=easygui.buttonbox("Wo ist die Kugel?","Finde die Kugel: Versuch {}".format(versuche),
+                  ["Klick mich!","Nein, hör nicht auf ihn! Klick mich!",
+                   "Wenn du mich klickst, wirst du es schaffen.","Komm zu mir!"],image="sea-serpent.gif")
+    richtig=random.choice(["Klick mich!","Nein, hör nicht auf ihn! Klick mich!",
+                   "Wenn du mich klickst, wirst du es schaffen.","Komm zu mir!"])
 
     if antwort ==richtig:
-         easygui.msgbox("Gut gemacht du hast die kugel gefunden.")
+         easygui.msgbox("Gut gemacht du hast die Kugel gefunden.")
          break
     else:
-         easygui.msgbox("Leider falsche wahl")
+         easygui.msgbox("Leider falsche Wahl")
 
 
 easygui.msgbox("Game Over",image="spectre.gif")
